e03267d8d1e5c896a62ea0eb62391c50a5ce494c
Fatoudrame <77timbs@gmail.com>
2023-04-30 11:15:39 +0000

    modifieé

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ var popup = document.getElementById('register-popup')
 var  createcompte = document.getElementById('create-account-button')
 var transparence = document.getElementById('overlay')
 var fermer = document.getElementById('close-register')
-
+//pour provoquer un comportement en js il faut cree un evenement.
 createcompte.addEventListener('click',(e)=> {
     e.preventDefault()
     popup.style.display = 'block'
